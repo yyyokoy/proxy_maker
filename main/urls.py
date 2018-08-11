e806b3ab304e4b00ac965eba19fd7c061c0ae2d0
YUI YOKOYAMA <>
2018-08-11 13:04:35 +0900

    テンプレを移植

---
+++ main/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from main import views
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', views.TopView.as_view(), name='top'),
+]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class TopView(TemplateView):
+    template_name = "main/top.html"
