47c56e77fa35976c9bcc4e1bb68a8644c2808a69
YUI YOKOYAMA <>
2018-08-25 14:12:24 +0900

    fix

--- PROXY_MAKER/wsgi.py
+++ PROXY_MAKER/wsgi.py
@@ -10,10 +10,10 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PROXY_MAKER.settings")
 
 application = get_wsgi_application()
 
+from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
